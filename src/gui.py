72a165943727091272ca0258d918d7a46a5f7f29
Rodolfo Vick <rodolfo.vick@gmail.com>
2015-12-20 22:20:53 -0200

    Adding GTK interface

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,71 @@
 #! /usr/bin/python
+
+from convert import imgConvert
+from gi.repository import Gtk, Gio
+
+class GuiWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title='PNG2PDF')
+        self.set_border_width(10)
+        self.set_default_size(400, 200)
+
+        self.fileList = []
+
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = 'PNG2PDF'
+        self.set_titlebar(hb)
+
+        addButton = Gtk.Button()
+        addButton.connect('clicked', self.addFile)
+        addIcon = Gio.ThemedIcon(name='list-add')
+        addImage = Gtk.Image.new_from_gicon(addIcon, Gtk.IconSize.BUTTON)
+        addButton.add(addImage)
+        hb.pack_start(addButton)
+
+        saveButton = Gtk.Button()
+        saveButton.connect('clicked', self.saveFile)
+        saveIcon = Gio.ThemedIcon(name='document-save')
+        saveImage = Gtk.Image.new_from_gicon(saveIcon, Gtk.IconSize.BUTTON)
+        saveButton.add(saveImage)
+        hb.pack_end(saveButton)
+
+    def addFile(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        #self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            self.fileList.append(dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def saveFile(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             "Select", Gtk.ResponseType.OK))
+        dialog.set_default_size(800, 400)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            fileName = dialog.get_filename()+'/ww.pdf'
+            imgConvert(self.fileList, fileName)
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+def guiStart():
+    win = GuiWindow()
+    win.connect('delete-event', Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+    return win
--- src/png2pdf.py
+++ src/png2pdf.py
@@ -1,10 +1,6 @@
 #! /usr/bin/python
 
-from convert import imgConvert
+from gui import guiStart
 
-list = ['/tmp/ww.jpg', '/tmp/ww2.jpg']
+win = guiStart()
 
-try:
-    imgConvert(list, '/tmp/ww.pdf')
-except IOError as e:
-    print('ERROR '+str(e.args))
